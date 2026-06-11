bfb496bcf6355bb2085424bf365b5c9c72e0ef12
Jasper <jasper.petit27@gmail.com>
2026-06-11 14:03:12 +0200

    modification du fichier de config database en utilisant DIR

--- config/DBconnect.php
+++ config/DBconnect.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $db = new PDO('sqlite:/var/www/html/Projet-Colis/data/database.db');
+        $db = new PDO('sqlite:'.__DIR__ . '/../data/database.db');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $db;
